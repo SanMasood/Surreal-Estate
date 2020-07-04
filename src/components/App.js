df9017ac89e087dd53220b8022167be7febff1bc
SanMasood <sanamasood.giki@gmail.com>
2020-07-04 12:50:46 +0100

    Some more cleanup; dependencies installation

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h2>Surreal Estate</h2>
-       
+        <h2>Surreal Estate</h2>
       </header>
     </div>
   );
