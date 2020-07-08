61100f3493d772ab4895bfb077d6aea0552db7df
SanMasood <sanamasood.giki@gmail.com>
2020-07-08 21:26:23 +0100

    Alert tests

--- src/tests/Alert.test.js
+++ src/tests/Alert.test.js
@@ -4,13 +4,28 @@ import Alert from '../components/Alert';
 
 test('displays error message', () => {
   const { getByText } = render(<Alert message="Error!" />);
-  //const alertMessageNode = component.getByText('Error!');
+  // const alertMessageNode = component.getByText('Error!');
 
   expect(getByText(/Error/).textContent).toBe('Error!');
 });
 
-/*test('displays success message',() => {
+test('displays success message', () => {
   const { getByText } = render(<Alert message="Success!" />);
 
-  expect(getByText(/success/).textContent).toBe('Success!');
-})*/
+  expect(getByText(/success/i).textContent).toBe('Success!');
+});
+
+test('does not render error or success if message props is empty', () => {
+  const { asFragment } = render(<Alert message="" />);
+  expect({ asFragment }).toMatchSnapshot();
+});
+
+test('renders error message if property not created', () => {
+  const { getByText, asFragment } = render(<Alert message="Error!" />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('renders success message if property created', () => {
+  const { getByText, asFragment }=render(<Alert message='Success!'/>);
+  expect(asFragment()).toMatchSnapshot();
+})
