3d663310f56840437f403f9a991f9e9330850e4e
SanMasood <sanamasood.giki@gmail.com>
2020-07-10 17:25:53 +0100

    Property cards styling

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import PropertyCard from './PropertyCard';
+import '../styles/Properties.css';
 import Alert from './Alert';
 
 const Properties = () => {
@@ -17,18 +18,20 @@ const Properties = () => {
 
   useEffect(() => {
     axios
-      .get('http://localhost:4000/api/v1/Propertyisting')
+      .get('http://localhost:4000/api/v1/PropertyListing')
       .then((response) => { setProperties(response.data); })
       .catch(({ message }) => { setAlert({ message: 'Error retrieving properties!' }); });
   }, []);
 
   return (
-    <div>
+    <div className="Properties">
       Properties Page
       <br />
       <div className="properties-alert"><Alert message={alert.message} /></div>
       {properties.map((property) => (
-        <PropertyCard key={property._id} {...property} />
+        <div key={property._id} className="col">
+          <PropertyCard {...property} />
+        </div>
       ))}
     </div>
   );
